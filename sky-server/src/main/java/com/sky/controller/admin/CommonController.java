3a14efcfacb623b925ea1a067d54dd8cd8b171be
Vincent-wwc <2352681526@qq.com>
2025-03-11 11:18:50 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,59 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import static com.sky.result.Result.success;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api("通用接口")
+@Slf4j
+public class CommonController {
+    @Autowired
+    AliOssUtil aliOssUtil;
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @ApiOperation("文件上传")
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传：{}", file);
+
+        try {
+            //获取文件原始名
+            String originalFilename = file.getOriginalFilename();
+
+            //获取文件拓展名
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+            //使用UUID生成新文件名
+            String fileName = UUID.randomUUID() + extension;
+
+            //文件上传
+            String uploaded = aliOssUtil.upload(file.getBytes(), fileName);
+
+            return Result.success(uploaded);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e);
+        }
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,52 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.FlavorMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    DishMapper dishMapper;
+
+    @Autowired
+    FlavorMapper flavorMapper;
+
+    /**
+     * 新增菜品与口味
+     * @param dishDTO
+     */
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+
+        //调用mapper，新增菜品
+        dishMapper.insert(dish);
+
+        //获取新增菜品的id
+        Long id = dish.getId();
+
+        //新增多条口味
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && !flavors.isEmpty()) {
+            flavors.forEach(flavor -> {
+                flavor.setDishId(id);
+            });
+
+            flavorMapper.insertBatch(flavors);
+        }
+
+    }
+}
